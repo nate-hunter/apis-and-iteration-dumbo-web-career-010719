5e86c04154119d5be21a2c5bf364086f6cccc03b
github username <github email address>
2019-01-08 16:05:04 -0500

    api pair project, Nate and Simon

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,42 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+# Nate Hunter & Simon Kong
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
+  film_array = []
+
+  # if response_hash["results"].fetch("name") == character_name
+  #   binding.pry
+
+
+
+  #end
+    response_hash.each do |title, data| # data is an array
+
+      #binding.pry
+
+      if title == "results"
+        data.each do |info| # info is a hash
+          #binding.pry
+          if info.fetch("name") == character_name # gets name
+            info["films"].each do |url|
+              #binding.pry
+              movie_url = RestClient.get(url)
+              movie_hash = JSON.parse(movie_url)
+              film_array << movie_hash
+            end # data do
+          end # if info
+        end #  do info
+      end # if title
+    end # do response_hash
+  film_array
+  #binding.pry
+end
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,16 +47,38 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
+
+
+#get_character_movies_from_api("R2-D2")
+
+
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  # This is the method in which you will iterate over that array of hashes to puts out movie information to the terminal.
+  # films.each do |title, info|
+  #   if title == "title"
+  #     puts info
+  #   end
+  # end
+
+  films.each do |element|
+    element.each do |title, name|
+    #binding.pry
+      if title == "title"
+        puts name
+      end
+    end
+  end
 end
+
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
+
+#show_character_movies("R2-D2")
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "hello"
 end
 
 def get_character_from_user
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  name = gets.chomp
+  name
 end
